b33a63e9655e0c4d3d5ef5c32bac325da515895c
Celso Dias <celsodias129@gmail.com>
2023-08-16 23:48:25 -0300

    feat: added initial state on open poup up

--- src/pop-up/pop-up.js
+++ src/pop-up/pop-up.js
@@ -1,11 +1,33 @@
-import { tab } from '../chrome'
+import { tab, storage } from '../chrome/index.js'
 
 const checkboxElement = document.getElementById('checkbox')
 
-checkbox.addEventListener('click', async () => {
+async function setInitialState() {
+  const value = await storage.local.get('formatJsonInTextOnClick')
+
+  const isEnable = value === 'enable'
+
+  checkboxElement.checked = isEnable
+
+  sendMessageToCurrentTab({ formatJsonInTextOnClick: value })
+}
+
+checkboxElement.addEventListener('click', () => {
+  const checkboxStatus = checkboxElement.checked ? 'enable' : 'disable'
+
+  const data = {
+    formatJsonInTextOnClick: checkboxStatus,
+  }
+
+  storage.local.set(data)
+
+  sendMessageToCurrentTab(data)
+})
+
+async function sendMessageToCurrentTab(data) {
   const currentTab = await tab.getCurrentTab()
 
-  tab.sendMessageToTabId(currentTab.id, {
-    formatJsonInTextOnClick: checkboxElement.checked ? 'enabled' : 'disabled',
-  })
-})
+  tab.sendMessageToTabId(currentTab.id, data)
+}
+
+setInitialState()
